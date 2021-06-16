348dc3b119b5e6e7c85d0f949cc612971486f168
Néstor Torres <alu0101021768@ull.edu.es>
2021-06-16 10:25:51 +0100

    Fixeando errores de importacion y exportación

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+          <div v-if="alert.message" :class="`alert ${alert.type}`">
+            {{ alert.message }}
+          </div>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  computed: {
+    alert() {
+      return this.$store.state.alert;
+    },
+  },
+  watch: {
+    $route() {
+      // clear alert on location change
+      this.$store.dispatch("alert/clear");
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
