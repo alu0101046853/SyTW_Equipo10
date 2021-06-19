7d1fa623bbb3ed13a13b67b676c2907d47c81317
edfigo98 <alu0101043838@ull.edu.es>
2021-06-19 18:50:33 +0100

    Cambios en la indentacion

--- client/src/components/UserForm.vue
+++ client/src/components/UserForm.vue
@@ -1,80 +1,80 @@
 <template>
   <div class="userform">
-  <v-app>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="user.username"
-      name="username"
-      label="Usuario"
-      required
-    ></v-text-field>
+    <v-app>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+      <v-text-field
+        v-model="user.username"
+        name="username"
+        label="Usuario"
+        required
+      ></v-text-field>
 
-    <v-text-field
-      v-model="user.name"
-      name="name"
-      label="Nombre"
-    ></v-text-field>
+      <v-text-field
+        v-model="user.name"
+        name="name"
+        label="Nombre"
+      ></v-text-field>
 
-    <v-text-field
-      v-model="user.email"
-      :rules="[rules.email]"
-      label="E-mail"
-      name="email"
-    ></v-text-field>
-
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="handleSubmit"
-    >
-      Actualizar
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="showdelete= !showdelete"
-    >
-    Borrar Usuario
-    </v-btn>
-    <div class="text-center" v-if="showdelete">
-    <v-sheet
-      class="px-7 pt-7 pb-4 mx-auto text-center d-inline-block"
-      color="blue-grey darken-3"
-      dark
-    >
-      <div class="grey--text text--lighten-1 text-body-2 mb-4">
-        ¿Estas seguro de que quieres borrar este usuario?
-      </div>
+      <v-text-field
+        v-model="user.email"
+        :rules="[rules.email]"
+        label="E-mail"
+        name="email"
+      ></v-text-field>
 
       <v-btn
-        :disabled="loading"
-        class="ma-1"
-        color="grey"
-        plain
-        @click="showdelete=false"
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="handleSubmit"
       >
-        Cancelar
+        Actualizar
       </v-btn>
 
       <v-btn
-        :loading="loading"
-        class="ma-1"
         color="error"
-        plain
-        @click="handleDelete"
+        class="mr-4"
+        @click="showdelete= !showdelete"
       >
-        Borrar
+        Borrar Usuario
       </v-btn>
-    </v-sheet>
-    </div>
-  </v-form>
-  </v-app>
+      <div class="text-center" v-if="showdelete">
+        <v-sheet
+          class="px-7 pt-7 pb-4 mx-auto text-center d-inline-block"
+          color="blue-grey darken-3"
+          dark
+        >
+        <div class="grey--text text--lighten-1 text-body-2 mb-4">
+          ¿Estas seguro de que quieres borrar este usuario?
+        </div>
+
+        <v-btn
+          :disabled="loading"
+          class="ma-1"
+          color="grey"
+          plain
+          @click="showdelete=false"
+        >
+          Cancelar
+        </v-btn>
+
+        <v-btn
+          :loading="loading"
+          class="ma-1"
+          color="error"
+          plain
+          @click="handleDelete"
+        >
+          Borrar
+        </v-btn>
+        </v-sheet>
+      </div>
+      </v-form>
+    </v-app>
   </div>
 </template>
 
