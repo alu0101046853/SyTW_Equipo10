825c98c80621ff2e366ce32b8f2d3a33ff30b94a
JoseCova <alu0101023663@ull.edu.es>
2021-06-20 09:46:45 +0100

    creada vista para los perfiles de usuario

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <div class="container">
+      <div class="user-data">
+        <v-img
+          lazy-src=""
+          max-height="289"
+          max-width="250"
+          class="user-image"
+          src="">
+        </v-img>
+        <div> {{ nombre }} </div>
+        <div> {{ username }} </div>
+      </div>
+      <div class="user-guachinches"></div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  data() {
+    return {
+      nombre: "",
+      username: "",
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+  }
+
+  .container .user-data {
+    display: flex;
+    flex-direction: column;
+    width: 18.75rem;
+    height: 12.5rem;
+  }
+
+  .container .user-data .user-image {
+    width:  60px;
+    height: 60px;
+  }
+
+  .container .user-guachinches {
+    margin-left: 20px;
+    width: 600px;
+    height: 300px;
+  }
+
+</style>
